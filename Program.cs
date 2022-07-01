ca17652725e86918f0847fe33f7a98b8b9be42f0
KrisJones91 <kristop.jones@gmail.com>
2022-07-01 08:34:29 -0600

    missing readkey added

--- Program.cs
+++ Program.cs
@@ -5,8 +5,14 @@ Random random = new Random();
 for (int i = 0; i < 10; i++)
 {
     Console.WriteLine("Press any Key to roll the dice");
+    Console.ReadKey();
+
+    playerRandomNum = random.Next(1, 7);
+
+    Console.WriteLine("You rolled a {0}", playerRandomNum);
 
 }
+Console.ReadKey();
 
 
 
